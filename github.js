1032e18046253d5887384c4f0d34be4eab7b9b9d
DarGrz <d.grzegorczyk@outlook.com>
2022-05-27 16:33:16 +0200

    final commit

--- github.js
+++ github.js
@@ -2,15 +2,23 @@ class GitHub {
     constructor() {
         this.client_id = '570dcb5716b84b0c54ec';
         this.client_secret = '4ef9511436c90fd14697186825c2a411c0c32da1';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc'
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+
         const profile = await profileResponse.json();
+        const repos = await reposResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
